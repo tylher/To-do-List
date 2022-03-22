084472cc0723f82cbadefbfa97b0178c78fe2897
Damola Adediran <tylher123@gmail.com>
2022-03-22 20:59:13 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,41 +1,43 @@
-import './style.css'
+import './style.css';
+
 const toDo = [
   {
-    description: "Arrange room",
+    description: 'Arrange room',
     completed: false,
     index: 1,
   },
   {
-    description: "Read a book",
+    description: 'Read a book',
     completed: false,
     index: 2,
   },
   {
-    description: "Study javascript",
+    description: 'Study javascript',
     completed: false,
     index: 3,
   },
   {
-    description: "Work on personal project",
+    description: 'Work on personal project',
     completed: false,
     index: 4,
   },
   {
-    description: "Talk to friends",
+    description: 'Talk to friends',
     completed: false,
     index: 5,
   },
 ];
 
-const listHolder = document.querySelector(".holder");
+const listHolder = document.querySelector('.holder');
 
 toDo.map((item) => {
-  let li = document.createElement("li");
-  li.classList.add("to-do-item");
+  const li = document.createElement('li');
+  li.classList.add('to-do-item');
   li.innerHTML = `<div>${
     item.completed
       ? '<i class="fa-solid fa-square-check"></i>'
       : '<i class="fa-solid fa-square"></i>'
   } <p>${item.description}</p></div> <i class="option fa-solid fa-ellipsis-vertical"></i>`;
   listHolder.appendChild(li);
+  return '';
 });
