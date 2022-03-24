5a266e9f4677e42af4a6851be757bc1a01ec753f
Damola Adediran <tylher123@gmail.com>
2022-03-24 21:07:32 +0100

    Revert "merge branches"
    This reverts commit 84137fcaa015c348e065541f3620b428848a9950.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.css';
+import ToDo from './toDo.js';
+
+const toDoObj = new ToDo();
+toDoObj.getTodo();
+toDoObj.addItem();
+toDoObj.removeItem();
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
