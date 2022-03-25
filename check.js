ce9364101e4591d351253ef8f64eb73182fe39f7
Damola Adediran <tylher123@gmail.com>
2022-03-25 12:32:36 +0100

    Updates

--- check.js
+++
@@ -1,8 +0,0 @@
-const checkItem = (todoList, index) => {
-  todoList = JSON.parse(localStorage.getItem('to-do'));
-  todoList[index].completed = !todoList[index].completed;
-  localStorage.setItem('to-do', JSON.stringify(todoList));
-  return todoList;
-};
-
-export default checkItem;
--- index.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-import ToDo from './toDo.js';
-
-const toDoObj = new ToDo();
-toDoObj.getTodo();
-toDoObj.addItem();
-toDoObj.removeItems();
-toDoObj.refreshList();
